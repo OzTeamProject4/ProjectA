f742415ca351f9f80acd575391dd83b095c73904
이진규 <gdjinkus@gmail.com>
2026-07-10 21:06:03 +0900

    feat: [AudioModel] 오디오 모델 추가

---
+++ Assets/Devs/LJK/AudioModel.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+
+public class AudioModel : INotifyPropertyChanged
+{
+    private float _bgmVolume;
+    private float _sfxVolume;
+
+    public AudioModel(AudioSettingData audioSettingData)
+    {
+        _bgmVolume = audioSettingData.BgmVolume;
+        _sfxVolume = audioSettingData.SfxVolume;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public float BgmVolume
+    {
+        get { return _bgmVolume; }
+        private set
+        {
+            if (_bgmVolume != value)
+            {
+                _bgmVolume = value;
+                OnPropertyChanged(nameof(BgmVolume));
+            }
+        }
+    }
+
+    public float SfxVolume
+    {
+        get { return _sfxVolume; }
+        private set
+        {
+            if (_sfxVolume != value)
+            {
+                _sfxVolume = value;
+                OnPropertyChanged(nameof(SfxVolume));
+            }
+        }
+    }
+
+    public void SetBgmVolume(float volume)
+    {
+        BgmVolume = volume;
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        SfxVolume = volume;
+    }
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
