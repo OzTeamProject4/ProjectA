ff25f3ca35e906908b6edcac7f0aa0007a66059e
이진규 <gdjinkus@gmail.com>
2026-07-10 21:06:44 +0900

    feat: [AudioViewModel] 오디오 뷰 모델 추가

---
+++ Assets/Devs/LJK/AudioViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+
+public class AudioViewModel
+{
+    private readonly AudioModel _audioModel;
+
+    public AudioViewModel(AudioSettingData audioSettingData)
+    {
+        _audioModel = new AudioModel(audioSettingData);
+        _audioModel.PropertyChanged += OnModelPropertyChanged;
+    }
+
+    public event Action<string> ModelPropertyChanged;
+
+    public float BgmVolume
+    {
+        get
+        {
+            return _audioModel.BgmVolume;
+        }
+    }
+
+    public float SfxVolume
+    {
+        get
+        {
+            return _audioModel.SfxVolume;
+        }
+    }
+
+    public void RequestSetBgmVolume(float volume)
+    {
+        _audioModel.SetBgmVolume(volume);
+    }
+
+    public void RequestSetSfxVolume(float volume)
+    {
+        _audioModel.SetSfxVolume(volume);
+    }
+
+    private void OnModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        ModelPropertyChanged?.Invoke(e.PropertyName);
+    }
+}
