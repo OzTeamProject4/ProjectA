642e72f9fe51026195825e05dc5d074111ac2bca
이진규 <gdjinkus@gmail.com>
2026-07-13 17:33:50 +0900

    feat: [Core] 어드레서블 키 문자열 수정

--- Assets/Devs/LJK/Core/AddressableKeys.cs
+++ Assets/Devs/LJK/Core/AddressableKeys.cs
@@ -3,7 +3,7 @@
     public static class Prefab
     {
         public const string UILayer = "Prefab/UILayer";
-        public const string AudioController = "Prefab/AudioController";
+        public const string AudioController = "Prefab/AudioView";
     }
 
     public static string GetUIKey(UIType uIType)
