d1026005e32f4617896f6637d5d039ceca9b7a04
김경훈 <r930324@gmail.com>
2026-07-14 17:49:17 +0900

    chore: [Grouping] 스크립트 폴더링 및 스텟 전달용 Mock Provider제작

---
+++ Assets/Devs/KKH/Scripts/Test/MockCharacterStatProvider.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public interface ICharacterStatProvider
+{
+    StatData GetFinalStats(string characterId);
+}
+
+public class MockCharacterStatProvider : ICharacterStatProvider
+{
+    private const int TempStar = 3;
+    private const int TempLevel = 1;
+
+    private readonly IGameDataProvider _dataProvider;
+
+    public MockCharacterStatProvider(IGameDataProvider dataProvider)
+    {
+        if (null == dataProvider)
+        {
+            Debug.LogError("[MockCharacterStatProvider] dataProvider 가 null 입니다.");
+        }
+
+        _dataProvider = dataProvider;
+    }
+
+    public StatData GetFinalStats(string characterId)
+    {
+        if (string.IsNullOrEmpty(characterId))
+        {
+            Debug.LogWarning("[MockCharacterStatProvider:GetFinalStats] characterId 가 비어 있습니다.");
+            return default;
+        }
+
+        CharacterStatData stat = _dataProvider.GetStat(characterId);
+        CharacterGradeData grade = _dataProvider.GetGrade(TempStar);
+
+        return StatCalculator.Calculate(stat, grade, TempLevel, default);
+    }
+}
+
