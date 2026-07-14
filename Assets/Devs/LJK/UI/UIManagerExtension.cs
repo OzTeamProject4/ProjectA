897dcbd05d9774b4c44d7a0525299c8a16782f8b
이진규 <gdjinkus@gmail.com>
2026-07-14 17:29:02 +0900

    feat: [UIManager] OpenUIAsync 메서드가 BaseUI 를 반환하도록 변경
    - 빈 BaseUI 클래스 추가

--- Assets/Devs/LJK/UI/UIManagerExtension.cs
+++ Assets/Devs/LJK/UI/UIManagerExtension.cs
@@ -5,7 +5,7 @@ public static class UIManagerExtension
 {
     public static async UniTask OpenTestUIAsync(this UIManager uiManager, CancellationToken cancellationToken = default)
     {
-        await uiManager.OpenTestRootAsync(UIType.Test, cancellationToken);
+        BaseUI baseUI = await uiManager.OpenTestRootAsync(UIType.Test, cancellationToken);
     }
 
     public static void CloseTestUI(this UIManager uiManager)
