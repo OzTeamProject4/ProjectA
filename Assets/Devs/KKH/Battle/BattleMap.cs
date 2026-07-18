8c2d808257e66d55a349b6887512a344cb53e4cd
김경훈 <r930324@gmail.com>
2026-07-18 23:10:21 +0900

    feat: [Stage] 전투맵 기초 작성 및 맵 경계 Collider / SkyBox 설정

---
+++ Assets/Devs/KKH/Battle/BattleMap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMap : MonoBehaviour
+{
+    [SerializeField] private Transform _playerSpawnPoint;
+    [SerializeField] private WaveTriggerZone[] _waveTriggerZones;
+
+    public Transform PlayerSpawnPoint
+    {
+        get { return _playerSpawnPoint; }
+    }
+
+    public IReadOnlyList<WaveTriggerZone> WaveTriggerZones
+    {
+        get { return _waveTriggerZones; }
+    }
+}
---
+++ Assets/Devs/KKH/Battle/WaveTriggerZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class WaveTriggerZone : MonoBehaviour
+{
+    [SerializeField] private int _waveIndex;
+    [SerializeField] private Transform[] _monsterSpawnPoints;
+    [SerializeField] private Transform _bossSpawnPoint;
+
+    public int WaveIndex
+    {
+        get { return _waveIndex; }
+    }
+
+    public IReadOnlyList<Transform> MonsterSpawnPoints
+    {
+        get { return _monsterSpawnPoints; }
+    }
+
+    public Transform BossSpawnPoint
+    {
+        get { return _bossSpawnPoint; }
+    }
+}
