562bc278a3feb1a5beda4711d366463d57db35dd
이진규 <gdjinkus@gmail.com>
2026-07-25 13:02:19 +0900

    feat: [EquipmentCraftPopup] 학생 육성 장비 제작 팝업UI 구조 MVVM으로 변경

---
+++ Assets/Devs/LJK/Fix/DataModel/EquipmentCraftListModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public class EquipmentCraftListModel
+{
+    private List<EquipmentCraftModel> _equipmentCraftModels;
+
+    public IReadOnlyList<EquipmentCraftModel> EquipmentCraftModels
+    {
+        get { return _equipmentCraftModels; }
+    }
+
+    public EquipmentCraftListModel(List<EquipmentCraftModel> equipmentCraftModels)
+    {
+        _equipmentCraftModels = equipmentCraftModels;
+    }
+
+    public IReadOnlyList<EquipmentCraftModel> GetEquipmentCraftsByEquipType(EquipType equipType)
+    {
+        return null;
+    }
+}
