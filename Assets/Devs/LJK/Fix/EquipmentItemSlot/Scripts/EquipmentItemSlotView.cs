529bc09b01f05ad6f909b3394280dd496856b706
이진규 <gdjinkus@gmail.com>
2026-07-25 18:30:58 +0900

    feat: [EquipmentItemSlot] 학생 육성 장비 인벤토리 슬롯 구조 MVVM으로 변경

---
+++ Assets/Devs/LJK/Fix/EquipmentItemSlot/Scripts/EquipmentItemSlotView.cs
@@ -0,0 +1,145 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipmentItemSlotView : MonoBehaviour
+{
+    [SerializeField] private RectTransform _rectTransfrom;
+    [SerializeField] private Button _slotButton;
+    [SerializeField] private Image _itemIconImage;
+    [SerializeField] private Image _equippedCharacterIconImage;
+    [SerializeField] private GameObject _selector;
+    [SerializeField] private TMP_Text _equippedText;
+
+    private EquipmentItemSlotViewModel _equipmentItemSlotViewModel;
+
+    public event Action<EquipmentModel, RectTransform> OnSlotClicked;
+
+    private CancellationTokenSource _disableCts;
+
+    private void Awake()
+    {
+        //TODO 추후 필드를 사용하면 추가
+        UnityUtil.ValidateReference(_itemIconImage, nameof(EquipmentItemSlotView), nameof(_itemIconImage));
+
+        _equipmentItemSlotViewModel = new EquipmentItemSlotViewModel();
+    }
+
+    public void OnEnable()
+    {
+        _disableCts = new CancellationTokenSource();
+
+        _slotButton.onClick.AddListener(HandleClickSelect);
+    }
+
+    public void OnDisable()
+    {
+        if (_disableCts == null)
+        {
+            _disableCts.Cancel();
+            _disableCts.Dispose();
+            _disableCts = null;
+        }
+
+        _slotButton.onClick.RemoveAllListeners();
+    }
+
+    public void SetModel(EquipmentModel equipmentModel)
+    {
+        _equipmentItemSlotViewModel.SetModel(equipmentModel);
+       
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        RefreshIconImageAsync().Forget();
+
+        _equipmentItemSlotViewModel.Refresh();
+    }
+
+    //TODO 기능 추가
+    private void OnPropertyChanged(string propertyName)
+    {
+        switch (propertyName)
+        {
+            //case nameof(_equipmentSlotViewModel.Name):
+            //    break;
+            //case nameof(_equipmentSlotViewModel.Count):
+            //    break;
+            //case nameof(_equipmentSlotViewModel.IconKey):
+            //    break;
+        }
+    }
+
+    private async UniTask RefreshIconImageAsync()
+    {
+        string iconKey = _equipmentItemSlotViewModel.IconKey;
+
+        if (string.IsNullOrWhiteSpace(iconKey))
+        {
+            return;
+        }
+
+        Sprite iconSprite = await GameManager.Instance.ResourceManager.LoadAssetAsync<Sprite>(iconKey, _disableCts.Token);
+
+        if (iconSprite == null)
+        {
+            return;
+        }
+
+        _itemIconImage.sprite = iconSprite;
+    }
+
+    //TODO 기능 추가
+    private async UniTaskVoid UpdateEquippedCharacterIconAsync()
+    {
+        //if (null == _equippedCharacterIconImage)
+        //{
+        //    return;
+        //}
+
+        //string iconPath = _equipmentSlotViewModel.EquippedCharacterIconPath;
+
+        //if (string.IsNullOrEmpty(iconPath))
+        //{
+        //    _equippedCharacterIconImage.enabled = false;
+        //    return;
+        //}
+
+        //try
+        //{
+        //    Sprite sprite = await GameManager.Instance.ResourceManager.LoadAssetAsync<Sprite>(iconPath, destroyCancellationToken);
+
+        //    if (null == sprite)
+        //    {
+        //        _equippedCharacterIconImage.enabled = false;
+        //        return;
+        //    }
+
+        //    _equippedCharacterIconImage.enabled = true;
+        //    _equippedCharacterIconImage.sprite = sprite;
+        //}
+        //catch (OperationCanceledException)
+        //{
+        //    // 오브젝트 파괴로 취소됨, 무시
+        //}
+        //catch (Exception exception)
+        //{
+        //    Debug.LogWarning($"[EquipmentListItemView] 장착 캐릭터 초상화 로드 실패. iconPath={iconPath}\n{exception}");
+        //}
+    }
+
+    private void HandleClickSelect()
+    {
+        if (OnSlotClicked == null)
+        {
+            return;
+        }
+
+        OnSlotClicked.Invoke(_equipmentItemSlotViewModel.EquipmentModel, _rectTransfrom);
+    }
+}
---
+++ Assets/Devs/LJK/Fix/EquipmentItemSlot/Scripts/EquipmentItemSlotViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+
+public class EquipmentItemSlotViewModel
+{
+    private EquipmentModel _equipmentModel;
+
+    public EquipmentModel EquipmentModel
+    {
+        get
+        {
+            return _equipmentModel;
+        }
+    }
+
+    public string IconKey
+    {
+        get
+        {
+            return _equipmentModel.IconKey;
+        }
+    }
+
+    public event Action<string> PropertyChanged;
+
+    public void SetModel(EquipmentModel equipmentModel)
+    {
+        if (_equipmentModel != null)
+        {
+            _equipmentModel.PropertyChanged -= OnPropertyChanged;
+        }
+
+        _equipmentModel = equipmentModel;
+        _equipmentModel.PropertyChanged += OnPropertyChanged;
+    }
+
+    public void Refresh()
+    {
+        _equipmentModel.NotifyAllProperties();
+    }
+
+    public void Dispose()
+    {
+        if (_equipmentModel != null)
+        {
+            _equipmentModel.PropertyChanged -= OnPropertyChanged;
+        }
+
+        _equipmentModel = null;
+    }
+
+    private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (PropertyChanged == null)
+        {
+            return;
+        }
+
+        PropertyChanged.Invoke(e.PropertyName);
+    }
+}
