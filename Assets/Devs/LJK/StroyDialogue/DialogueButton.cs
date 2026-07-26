488c006fde2a39f4fd3e305078a9cabf93807964
이진규 <gdjinkus@gmail.com>
2026-07-27 00:55:05 +0900

    feat: [Dialogue] 다이얼로그 뷰와 뷰 모델 추가
    - 대화 이름과 내용 변경 로직 추가
    - 클릭시 다음 대화 로직 추가

---
+++ Assets/Devs/LJK/StroyDialogue/DialogueButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class DialogueButton : BaseButton
+{
+    public event Action ButtonClicked;
+
+    protected override void OnButtonClick()
+    {
+        if (ButtonClicked == null)
+        {
+            return;
+        }
+
+        ButtonClicked.Invoke();
+    }
+}
---
+++ Assets/Devs/LJK/StroyDialogue/DialogueViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+
+public class DialogueViewModel
+{
+    private DialogueModel _dialogueModel;
+
+    public string SpeakerNameText
+    {
+        get 
+        {
+            return _dialogueModel.SpeakerNameText; 
+        }
+    }
+
+    public string DialogueText
+    {
+        get 
+        {
+            return _dialogueModel.DialogueText;
+        }
+    }
+
+    public event Action<string> ModelPropertyChanged;
+
+    public DialogueViewModel()
+    {
+        _dialogueModel = NetworkManager.Instance.DialogueModel;
+    }
+
+    public void OnEnable()
+    {
+        _dialogueModel.PropertyChanged += OnModelPropertyChanged;
+    }
+
+    public void OnDisable()
+    {
+        _dialogueModel.PropertyChanged -= OnModelPropertyChanged;
+    }
+
+    public void Dispose()
+    {
+        _dialogueModel = null;
+    }
+
+    public void Refresh()
+    {
+        _dialogueModel.NotifyAllProperties();
+    }
+
+    private void OnModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (ModelPropertyChanged == null)
+        {
+            return;
+        }
+
+        ModelPropertyChanged.Invoke(e.PropertyName);
+    }
+}
