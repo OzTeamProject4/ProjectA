4575a236114cb752884150382e5cce308e112045
이진규 <gdjinkus@gmail.com>
2026-08-01 20:07:13 +0900

    feat: [Dialogue] 다이얼로그 로그 기능 추가

---
+++ Assets/Devs/LJK/StroyDialogue/Log/DialogueHistorySlotView.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class DialogueHistorySlotView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _nameText;
+    [SerializeField] private TMP_Text _dialoguetext;
+
+    public void UpdateSlot(DialogueData dialogueData)
+    {
+        _nameText.text = dialogueData.SpeakerName;
+        _dialoguetext.text = dialogueData.Text;
+    }
+
+    public void SetActive(bool active)
+    {
+        if (gameObject.activeSelf == active)
+        {
+            return;
+        }
+
+        gameObject.SetActive(active);
+    }
+}
